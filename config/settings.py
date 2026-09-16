91f33209e2ff07e397ee215f4b9fff9f40db47f6
WildanFrananda <wildanrailfanskrl@gmail.com>
2026-09-16 13:58:01 +0700

    feat: Add database connection settings for maximum age and health checks

--- config/settings.py
+++ config/settings.py
@@ -94,6 +94,8 @@ DATABASES = {
         'PASSWORD': _require_env("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST", "localhost"),
         'PORT': os.environ.get("DB_PORT", "5432"),
+        'CONN_MAX_AGE': int(os.environ.get("DB_CONN_MAX_AGE", "60")),
+        'CONN_HEALTH_CHECKS': True,
     }
 }
 
